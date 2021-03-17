5ac6ea5756fe2f33b669ad87380b3f8142020797
davwys <david.wyss@hotmail.ch>
2021-03-17 16:14:42 +0100

    Fullscreen Map

--- frontend/app/src/pages/MainPage.vue
+++ frontend/app/src/pages/MainPage.vue
@@ -1,20 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <GmapMap
-        :center="{lat:10, lng:10}"
-        :zoom="7"
-        map-type-id="terrain"
-        style="width: 500px; height: 300px"
+  <q-page>
+    <div
+      class="window-height"
+      style="padding-bottom: 50px"
     >
-<!--      <GmapMarker-->
-<!--          :key="index"-->
-<!--          v-for="(m, index) in markers"-->
-<!--          :position="m.position"-->
-<!--          :clickable="true"-->
-<!--          :draggable="true"-->
-<!--          @click="center=m.position"-->
-<!--      />-->
-    </GmapMap>
+      <GmapMap
+          :center="{lat:40.730610, lng:-73.935242}"
+          :zoom="12"
+          map-type-id="roadmap"
+          style="width: 100%; height: 100%"
+          :options="{
+           zoomControl: true,
+           mapTypeControl: false,
+           scaleControl: false,
+           streetViewControl: false,
+           rotateControl: false,
+           fullscreenControl: false,
+           disableDefaultUI: false
+         }"
+      >
+        <!--      <GmapMarker-->
+        <!--          :key="index"-->
+        <!--          v-for="(m, index) in markers"-->
+        <!--          :position="m.position"-->
+        <!--          :clickable="true"-->
+        <!--          :draggable="true"-->
+        <!--          @click="center=m.position"-->
+        <!--      />-->
+      </GmapMap>
+    </div>
   </q-page>
 </template>
 
