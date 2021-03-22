a84fadb364bb71d24c4ebbf1db4bc1d7d5e4635a
johannschwabe <johann.schwabe@uzh.ch>
2021-03-22 15:18:54 +0100

    setup tests

--- frontend/app/src/pages/MainPage.vue
+++ frontend/app/src/pages/MainPage.vue
@@ -5,19 +5,19 @@
       style="padding-bottom: 50px"
     >
       <GmapMap
-          :center="{lat:40.730610, lng:-73.935242}"
-          :zoom="12"
-          map-type-id="roadmap"
-          style="width: 100%; height: 100%"
-          :options="{
-           zoomControl: true,
-           mapTypeControl: false,
-           scaleControl: false,
-           streetViewControl: false,
-           rotateControl: false,
-           fullscreenControl: false,
-           disableDefaultUI: false
-         }"
+        :center="{lat:40.730610, lng:-73.935242}"
+        :zoom="12"
+        map-type-id="roadmap"
+        style="width: 100%; height: 100%"
+        :options="{
+          zoomControl: true,
+          mapTypeControl: false,
+          scaleControl: false,
+          streetViewControl: false,
+          rotateControl: false,
+          fullscreenControl: false,
+          disableDefaultUI: false,
+        }"
       >
         <!--      <GmapMarker-->
         <!--          :key="index"-->
@@ -34,8 +34,8 @@
 
 <script>
 export default {
-  name: "MainPage"
-}
+  name: "MainPage",
+};
 </script>
 
 <style scoped>
---
+++ frontend/app/src/test/example.test.js
@@ -0,0 +1,5 @@
+describe("1 example", () => {
+  test("1.1 example", () => {
+    expect(false);
+  });
+});
