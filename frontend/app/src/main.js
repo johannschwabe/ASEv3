4820216ef3c119bc4f0f69e24ade294354e8766b
johannschwabe <johann.schwabe@uzh.ch>
2021-03-22 16:13:28 +0100

    fixed eslint

--- frontend/app/src/main.js
+++ frontend/app/src/main.js
@@ -1,36 +1,36 @@
-import Vue from 'vue'
-import App from './App.vue'
-import * as VueGoogleMaps from 'gmap-vue'
-import './quasar'
+import Vue from "vue";
+import * as VueGoogleMaps from "gmap-vue";
+import App from "./App.vue";
+import "./quasar.js";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(VueGoogleMaps, {
   load: {
     key: "***REMOVED***",
-    //libraries: 'places', // This is required if you use the Autocomplete plugin
+    // libraries: 'places', // This is required if you use the Autocomplete plugin
     // OR: libraries: 'places,drawing'
     // OR: libraries: 'places,drawing,visualization'
     // (as you require)
 
-    //// If you want to set the version, you can do so:
+    /// / If you want to set the version, you can do so:
     // v: '3.26',
   },
 
-  //// If you intend to programmatically custom event listener code
-  //// (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
-  //// instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
-  //// you might need to turn this on.
+  /// / If you intend to programmatically custom event listener code
+  /// / (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
+  /// / instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
+  /// / you might need to turn this on.
   // autobindAllEvents: false,
 
-  //// If you want to manually install components, e.g.
-  //// import {GmapMarker} from 'gmap-vue/src/components/marker'
-  //// Vue.component('GmapMarker', GmapMarker)
-  //// then set installComponents to 'false'.
-  //// If you want to automatically install all the components this property must be set to 'true':
-  installComponents: true
-})
+  /// / If you want to manually install components, e.g.
+  /// / import {GmapMarker} from 'gmap-vue/src/components/marker'
+  /// / Vue.component('GmapMarker', GmapMarker)
+  /// / then set installComponents to 'false'.
+  /// / If you want to automatically install all the components this property must be set to 'true':
+  installComponents: true,
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
