f847101f66a8620e1d34aabb218a7913a6b5d96b
johannschwabe <johann.schwabe@uzh.ch>
2021-04-11 20:06:12 +0200

    i try

--- data/init_sales.sql
+++ data/init_sales.sql
@@ -1,4 +1,12 @@
-USE app_db;
+CREATE DATABASE app_db;
+CREATE TABLE coordinates (
+    id_sale VARCHAR(255) NOT NULL,
+    lat REAL,
+    `long` REAL,
+    returned_street_name VARCHAR(255),
+    returned_zip_code VARCHAR(255),
+    PRIMARY KEY (id_sale)
+);
 LOAD DATA INFILE '/data/coords_nyc_api.csv' INTO TABLE coordinates
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '/n'
