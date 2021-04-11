53d20b9c4c947544181242120448b466219fd750
johannschwabe <johann.schwabe@uzh.ch>
2021-04-11 17:04:34 +0200

    i doubt it

---
+++ data/init_sales.sql
@@ -0,0 +1,12 @@
+CREATE TABLE coordinates (
+    id_sale VARCHAR(255) NOT NULL
+    lat REAL
+    long REAL
+    returned_street_name VARCHAR(255)
+    returned_zip_code VARCHAR(255)
+    PRIMARY KEY (id_sale)
+)
+LOAD DATA INFILE '/data/coords_nyc_api.csv' INTO TABLE coordinates
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '/n'
+IGNORE 1 ROWS;
