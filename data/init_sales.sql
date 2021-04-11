d17f0f45d3c447b839b4f4ce6718dd8aadbdaedc
johannschwabe <johann.schwabe@uzh.ch>
2021-04-11 17:57:31 +0200

    dito

--- data/init_sales.sql
+++ data/init_sales.sql
@@ -6,7 +6,7 @@ CREATE TABLE coordinates (
     returned_zip_code VARCHAR(255),
     PRIMARY KEY (id_sale)
 );
--- LOAD DATA INFILE '/data/coords_nyc_api.csv' INTO TABLE coordinates
--- FIELDS TERMINATED BY ','
--- LINES TERMINATED BY '/n'
--- IGNORE 1 ROWS;
+LOAD DATA INFILE '/data/coords_nyc_api.csv' INTO TABLE coordinates
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '/n'
+IGNORE 1 ROWS;
