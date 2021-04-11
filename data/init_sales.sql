450cd2e75384a590d376ea40569164adfcf012e7
johannschwabe <johann.schwabe@uzh.ch>
2021-04-11 20:01:05 +0200

    changes

--- data/init_sales.sql
+++ data/init_sales.sql
@@ -1,11 +1,4 @@
-CREATE TABLE coordinates (
-    id_sale VARCHAR(255) NOT NULL,
-    lat REAL,
-    `long` REAL,
-    returned_street_name VARCHAR(255),
-    returned_zip_code VARCHAR(255),
-    PRIMARY KEY (id_sale)
-);
+USE app_db;
 LOAD DATA INFILE '/data/coords_nyc_api.csv' INTO TABLE coordinates
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '/n'
