2964d274010097130e1205d600e6ce6ec8c9c857
thommann <thomasrolf.mannhart@uzh.ch>
2021-05-13 19:10:47 +0200

    Backend: clean up

--- backend/src/test/java/ch/ase21/backend/service/SalesServiceTests.java
+++ backend/src/test/java/ch/ase21/backend/service/SalesServiceTests.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 
