7f248e49e2ec0090acf73e4c813e402ebc96cf1b
johannschwabe <johann.schwabe@uzh.ch>
2021-05-23 16:07:32 +0200

    Document tests

--- frontend/src/test/PropertyCard.test.js
+++ frontend/src/test/PropertyCard.test.js
@@ -1,6 +1,9 @@
 import PropertyCard from "../components/PropertyCard.vue"
 import {mount} from "@vue/test-utils";
 
+/**
+ * Test if the component PropertyCard Mounts
+ */
 describe("propertyCardMounts",()=>{
   test("1.1 mounts",()=>{
     const card = mount(PropertyCard, {
