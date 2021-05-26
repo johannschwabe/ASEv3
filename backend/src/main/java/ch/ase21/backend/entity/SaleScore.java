4b9f56d59b5c613caed355eff3e5b7f1d3080ad5
thommann <thomasrolf.mannhart@uzh.ch>
2021-05-26 09:58:54 +0200

    Backend: Return address, price and neighbourhood with the score

--- backend/src/main/java/ch/ase21/backend/entity/SaleScore.java
+++ backend/src/main/java/ch/ase21/backend/entity/SaleScore.java
@@ -3,17 +3,35 @@ package ch.ase21.backend.entity;
 public class SaleScore {
   private final String id;
   private final Double score;
+  private final String address;
+  private final Integer price;
+  private final String neighbourhood;
 
-  public SaleScore(String id, Double score) {
+  public SaleScore(String id, Double score, String address, Integer price, String neighbourhood) {
     this.id = id;
     this.score = score;
+    this.address = address;
+    this.price = price;
+    this.neighbourhood = neighbourhood;
+  }
+
+  public String getId() {
+    return id;
   }
 
   public Double getScore() {
     return score;
   }
 
-  public String getId() {
-    return id;
+  public String getAddress(){
+    return address;
+  }
+
+  public Integer getPrice(){
+    return price;
+  }
+
+  public String getNeighbourhood(){
+    return neighbourhood;
   }
 }
