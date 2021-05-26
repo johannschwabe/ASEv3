bb13c4ed40e5ff74bac2794a0abe85702f4da1d5
davwys <david.wyss@hotmail.ch>
2021-05-26 10:30:12 +0200

    Fetching change

--- frontend/src/components/view/TableDialog.vue
+++ frontend/src/components/view/TableDialog.vue
@@ -65,7 +65,7 @@ export default {
         data: {
           query: `
             {
-              allScores(){
+              allScores{
                 id
                 score
                 price
