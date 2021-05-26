7d3cdd625b6e19bcf7cb11f2a97d6351f87a829d
thommann <thomasrolf.mannhart@uzh.ch>
2021-05-26 11:50:41 +0200

    Fix database script

--- scripts/database.sh
+++ scripts/database.sh
@@ -1,2 +1,2 @@
-mysql -h localhost:8585 -u db_user -pdb_user_pass app_db < data\app_db-_localhost_8585_airbnbdb.sql
-mysql -h localhost:8686 -u db_user -pdb_user_pass app_db < data\app_db-_localhost_8585_salesdb.sql
+mysql -h localhost -P 8585 -u db_user -pdb_user_pass app_db < data\app_db-_localhost_8585_airbnbdb.sql
+mysql -h localhost -P 8686 -u db_user -pdb_user_pass app_db < data\app_db-_localhost_8686_salesdb.sql
